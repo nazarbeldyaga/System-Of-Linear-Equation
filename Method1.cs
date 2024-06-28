36bd33d4b7c259614017fa2b659f193981fc1233
Назар Бедьдяга <nazar.beldyaga@gmail.com>
2024-06-29 01:14:56 +0300

    1

--- Method1.cs
+++ Method1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System_Of_Linear_Equation;
 
 public static class Method1
 {
@@ -8,7 +7,7 @@ public static class Method1
         int operations = 0;
         (double[,] L, double[,] U, int[] P) = LUPDecomposition(A, ref operations);
         double[,] Y = SolveForY(L, B, P, ref operations);
-        double[,] X = SolveForX(U, Y, ref operations);        
+        double[,] X = SolveForX(U, Y, ref operations);
         return (X, L, U, P, Y, operations);
     }
 
@@ -17,101 +16,81 @@ public static class Method1
         int n = A.GetLength(0);
         double[,] L = new double[n, n];
         double[,] U = new double[n, n];
-        int[] P = new int[n]; //Вектор перестановок
+        int[] P = new int[n];
 
         for (int i = 0; i < n; i++)
         {
-            for (int m = 2; m <= n; m++)
+            P[i] = i;
+        }
+
+        double[,] C = (double[,])A.Clone();
+
+        for (int i = 0; i < n; i++)
+        {
+            double pivotValue = 0;
+            int pivot = -1;
+            for (int row = i; row < n; row++)
             {
-                double[,] minor = Form1.GetMinor(A, m);
-                double det = Form1.CalculateDeterminant(minor);
-                if (det == 0)
+                if (Math.Abs(C[row, i]) > pivotValue)
                 {
-                    for (int l = n; l != 0; l--)
-                    {
-                        P[n - l] = l - 1;
-                    }
-                    break;
-                }
-                else
-                {
-                    P[i] = i;
+                    pivotValue = Math.Abs(C[row, i]);
+                    pivot = row;
                 }
             }
-             // Веткор преестановок - вектор з поточним номером рядка
 
-            for (int j = 0; j <= i; j++)
+            if (pivotValue != 0)
             {
-                double sum = 0;
+                SwapRows(ref P, i, pivot);
+                SwapRows(ref C, i, pivot);
 
-                for (int k = 0; k < j; k++)
+                for (int j = i + 1; j < n; j++)
                 {
-                    operations += 2;
-                    sum += L[i, k] * U[k, j];
-                }
-                operations += 2;
-                L[i, j] = A[P[i], j] - sum;
-                operations++;
-                if (i == j)
-                    U[i, j] = 1;
-                else
-                {
-                    sum = 0;
-                    for (int k = 0; k < i; k++)
+                    C[j, i] /= C[i, i];
+                    operations++;
+                    for (int k = i + 1; k < n; k++)
                     {
+                        C[j, k] -= C[j, i] * C[i, k];
                         operations += 2;
-                        sum += L[j, k] * U[k, i];
                     }
-                    operations += 3;
-                    U[j, i] = (A[P[j], i] - sum) / L[j, j];
                 }
-            }
-
-            
-            //знайти рядок, який має максимальне абсолютне значення в поточному стовпчику, починаючи з рядка i.
-            operations++;
-            double maxVal = Math.Abs(L[i, i]); // Ініціалізуємо maxVal як абсолютне значення поточного діагонального елемента
-            int pivotRow = i; // Ініціалізуємо pivotRow як поточний рядок
-            for (int k = i + 1; k < n; k++) // Цикл для перегляду всіх рядків нижче поточного
-            {
-                operations +=2;
-                double absVal = Math.Abs(L[k, i]); // Обчислюємо абсолютне значення елемента в стовпчику і рядку k
-                if (absVal > maxVal) // Якщо знайдене значення більше за поточне максимальне
-                {
-                    maxVal = absVal; // Оновлюємо максимальне значення
-                    pivotRow = k; // Оновлюємо індекс pivotRow
-                }
-            }
-
-            // Міняємо рядки у L та оновлюємо матрицю перестановок
-            operations++;
-            if (pivotRow != i)
-            {
-                SwapRows(ref L, i, pivotRow);
-                Swap(ref P[i], ref P[pivotRow]);
             }
         }
-        
+
+        // Разделение C на L и U
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i > j)
+                    L[i, j] = C[i, j];
+                else
+                {
+                    U[i, j] = C[i, j];
+                    if (i == j)
+                        L[i, j] = 1;
+                }
+            }
+        }
 
         return (L, U, P);
     }
 
+    private static void SwapRows(ref int[] P, int i, int j)
+    {
+        int temp = P[i];
+        P[i] = P[j];
+        P[j] = temp;
+    }
+
     private static void SwapRows(ref double[,] matrix, int row1, int row2)
     {
-        int n = matrix.GetLength(0);
+        int n = matrix.GetLength(1);
         for (int j = 0; j < n; j++)
         {
             double temp = matrix[row1, j];
             matrix[row1, j] = matrix[row2, j];
             matrix[row2, j] = temp;
         }
-    }
-    //міняємо відповідні елементи у матриці перестановок
-    private static void Swap(ref int a, ref int b)
-    {
-        int temp = a;
-        a = b;
-        b = temp;
     }
 
     public static double[,] SolveForY(double[,] L, double[,] B, int[] P, ref int operations)
@@ -128,11 +107,11 @@ public static class Method1
                 sum += L[i, k] * Y[k, 0];
             }
             operations += 2;
-            Y[i, 0] = (B[0, P[i]] - sum) / L[i, i];
+            Y[i, 0] = B[0, P[i]] - sum;
         }
+
         return Y;
     }
-
 
     public static double[,] SolveForX(double[,] U, double[,] Y, ref int operations)
     {
@@ -148,9 +127,9 @@ public static class Method1
                 sum += U[i, k] * X[k, 0];
             }
             operations++;
-            X[i, 0] = Y[i, 0] - sum;
+            X[i, 0] = (Y[i, 0] - sum) / U[i, i];
         }
+
         return X;
     }
-
 }
